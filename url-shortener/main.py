226596cd48672acabb6e0d3ada65714f3e2bd1fd
Yaroslav <iaroslav.bierioza@gmail.com>
2025-06-01 15:10:57 +0300

    add root route with greeting and docs link

--- url-shortener/main.py
+++ url-shortener/main.py
@@ -1,6 +1,23 @@
-from fastapi import FastAPI
+from fastapi import (
+    FastAPI,
+    Request,
+)
 
 app = FastAPI(
     title="URL Shortener",
 )
 
+
+@app.get("/")
+def read_root(
+    request: Request,
+    name: str = "World",
+):
+    docs_url = request.url.replace(
+        path="/docs",
+        query="",
+    )
+    return {
+        "message": f"Hello {name}",
+        "docs": str(docs_url),
+    }
