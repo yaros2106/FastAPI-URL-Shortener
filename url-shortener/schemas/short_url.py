f7afcc66c3e515f1ddc8ef3c6fc2fd34cfef4a7d
Yaroslav <iaroslav.bierioza@gmail.com>
2025-06-05 13:57:24 +0300

    create ShortUrlCreate model with slug and length validation

--- url-shortener/schemas/short_url.py
+++ url-shortener/schemas/short_url.py
@@ -1,9 +1,23 @@
+from typing import Annotated
+
+from annotated_types import Len
 from pydantic import BaseModel, AnyHttpUrl
 
 
 class ShortUrlBase(BaseModel):
     target_url: AnyHttpUrl
     slug: str
+
+
+class ShortUrlCreate(ShortUrlBase):
+    """
+    Модель для создания сокращенной ссылки
+    """
+
+    slug: Annotated[
+        str,
+        Len(min_length=3, max_length=10),
+    ]
 
 
 class ShortUrl(ShortUrlBase):
