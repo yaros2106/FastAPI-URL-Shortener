d42a29d218a2b5f07afc1d88ddb1790a8f97315d
Yaroslav <iaroslav.bierioza@gmail.com>
2025-07-11 13:25:21 +0300

    pytest class test

--- url-shortener/testing/test_api/test_api_v1/test_short_urls/test_dependencies.py
+++ url-shortener/testing/test_api/test_api_v1/test_short_urls/test_dependencies.py
@@ -1,10 +1,15 @@
 from api.api_v1.short_urls.dependencies import UNSAFE_METHODS
 
 
-def test_unsafe_methods_doesnt_contain_safe_methods() -> None:
-    safe_methods = {
-        "GET",
-        "HEAD",
-        "OPTIONS",
-    }
-    assert not UNSAFE_METHODS & safe_methods
+class TestUnsafeMethods:
+
+    def test_dont_contain_save_methods(self) -> None:
+        save_methods = {
+            "GET",
+            "HEAD",
+            "OPTIONS",
+        }
+        assert not UNSAFE_METHODS & save_methods
+
+    def test_all_methods_are_upper(self) -> None:
+        assert all(method.isupper() for method in UNSAFE_METHODS)
