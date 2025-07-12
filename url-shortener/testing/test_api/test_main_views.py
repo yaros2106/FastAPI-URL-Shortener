f02487608d9a191c0abe43fd1fc22cb06aad96e9
Yaroslav <iaroslav.bierioza@gmail.com>
2025-07-12 19:27:14 +0300

    parametrizing tests

--- url-shortener/testing/test_api/test_main_views.py
+++ url-shortener/testing/test_api/test_main_views.py
@@ -1,3 +1,4 @@
+import pytest
 from fastapi import status
 from fastapi.testclient import TestClient
 
@@ -7,6 +8,24 @@ client = TestClient(app)
 
 
 def test_root_view() -> None:
+    response = client.get("/")
+    assert response.status_code == status.HTTP_200_OK, response.text
+    response_data = response.json()
+    expected_message = "Hello World"
+    assert response_data["message"] == expected_message, response_data
+
+
+@pytest.mark.parametrize(
+    "name",
+    (
+        # TODO: fake data
+        "yaros",
+        "",
+        "yaros bel",
+        "!@#$%",
+    ),
+)
+def test_root_view_custom_name(name: str) -> None:
     # TODO: fake data
     name = "yaros"
     query = {"name": name}
