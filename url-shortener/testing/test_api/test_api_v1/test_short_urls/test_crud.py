edda3789737fe236f96b9afc76353185a4a759b1
Yaroslav <iaroslav.bierioza@gmail.com>
2025-07-13 14:15:57 +0300

    first pytest.fixture

--- url-shortener/testing/test_api/test_api_v1/test_short_urls/test_crud.py
+++ url-shortener/testing/test_api/test_api_v1/test_short_urls/test_crud.py
@@ -31,6 +31,11 @@ def create_short_url() -> ShortUrl:
     return storage.create(short_url_in)
 
 
+@pytest.fixture
+def short_url() -> ShortUrl:
+    return create_short_url()
+
+
 class ShortUrlsStorageUpdateTestCase(TestCase):
     def setUp(self) -> None:
         self.short_url = create_short_url()
@@ -110,9 +115,8 @@ class ShortUrlsStorageGetShortUrlsTestCase(TestCase):
                 )
 
 
-def test_create_or_raise_if_exists() -> None:
-    existing_short_url = create_short_url()
-    short_url_create = ShortUrlCreate(**existing_short_url.model_dump())
+def test_create_or_raise_if_exists(short_url: ShortUrl) -> None:
+    short_url_create = ShortUrlCreate(**short_url.model_dump())
     with pytest.raises(
         ShortUrlAlreadyExistsError,
         match=short_url_create.slug,
