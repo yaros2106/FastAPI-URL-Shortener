4195bed4da5eddbc9573a03a16f607a8c22a1ad9
Yaroslav <iaroslav.bierioza@gmail.com>
2025-07-13 13:25:45 +0300

    pytest.raises for existing short url

--- url-shortener/testing/test_api/test_api_v1/test_short_urls/test_crud.py
+++ url-shortener/testing/test_api/test_api_v1/test_short_urls/test_crud.py
@@ -3,7 +3,12 @@ import string
 from typing import ClassVar
 from unittest import TestCase
 
-from api.api_v1.short_urls.crud import storage
+import pytest
+
+from api.api_v1.short_urls.crud import (
+    ShortUrlAlreadyExistsError,
+    storage,
+)
 from schemas.short_url import (
     ShortUrl,
     ShortUrlCreate,
@@ -103,3 +108,15 @@ class ShortUrlsStorageGetShortUrlsTestCase(TestCase):
                     short_url,
                     db_short_url,
                 )
+
+
+def test_create_or_raise_if_exists() -> None:
+    existing_short_url = create_short_url()
+    short_url_create = ShortUrlCreate(**existing_short_url.model_dump())
+    with pytest.raises(
+        ShortUrlAlreadyExistsError,
+        match=short_url_create.slug,
+    ) as exc_info:
+        storage.create_or_raise_if_exists(short_url_create)
+
+    assert exc_info.value.args[0] == short_url_create.slug
