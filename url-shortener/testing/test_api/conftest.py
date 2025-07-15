7d5c288b1986e179168659309921d25ca8b2bdb1
Yaroslav <iaroslav.bierioza@gmail.com>
2025-07-15 13:35:42 +0300

    api tests for creation, nested fixtures, scopes

--- url-shortener/testing/test_api/conftest.py
+++ url-shortener/testing/test_api/conftest.py
@@ -3,10 +3,27 @@ from collections.abc import Generator
 import pytest
 from fastapi.testclient import TestClient
 
+from api.api_v1.auth.services import redis_tokens
 from main import app
 
 
 @pytest.fixture()
 def client() -> Generator[TestClient]:
     with TestClient(app) as client:
+        yield client
+
+
+@pytest.fixture(scope="module")
+def auth_token() -> Generator[str]:
+    token = redis_tokens.generate_and_save_token()
+    yield token
+    redis_tokens.delete_token(token)
+
+
+@pytest.fixture(scope="module")
+def auth_client(
+    auth_token: str,
+) -> Generator[TestClient]:
+    headers = {"Authorization": f"Bearer {auth_token}"}
+    with TestClient(app, headers=headers) as client:
         yield client
---
+++ url-shortener/testing/test_api/test_api_v1/test_short_urls/test_views/test_list_views/test_create.py
@@ -0,0 +1,28 @@
+import random
+import string
+
+from fastapi import status
+from fastapi.testclient import TestClient
+
+from main import app
+from schemas.short_url import ShortUrlCreate
+
+
+def test_create_short_url(auth_client: TestClient) -> None:
+    url = app.url_path_for("create_short_url")
+    short_url_create = ShortUrlCreate(
+        slug="".join(
+            random.choices(
+                string.ascii_letters,
+                k=8,
+            ),
+        ),
+        description="A short url",
+        target_url="https://example.com",
+    )
+    data: dict[str, str] = short_url_create.model_dump(mode="json")
+    response = auth_client.post(url=url, json=data)
+    assert response.status_code == status.HTTP_201_CREATED, response.text
+    response_data = response.json()
+    received_values = ShortUrlCreate(**response_data)
+    assert received_values == short_url_create, response_data
