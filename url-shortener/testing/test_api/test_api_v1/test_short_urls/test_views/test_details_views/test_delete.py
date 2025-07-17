e2f36c996dff92cfe4ac1968881143ee913b388c
Yaroslav <iaroslav.bierioza@gmail.com>
2025-07-17 14:02:39 +0300

    pytest parametrize fixture for delete short-url

---
+++ url-shortener/testing/test_api/test_api_v1/test_short_urls/test_views/test_details_views/test_delete.py
@@ -0,0 +1,43 @@
+import pytest
+from _pytest.fixtures import SubRequest
+from fastapi import status
+from fastapi.testclient import TestClient
+
+from api.api_v1.short_urls.crud import storage
+from main import app
+from schemas.short_url import ShortUrl, ShortUrlCreate
+
+
+def create_short_url(slug: str) -> ShortUrl:
+    short_url_in = ShortUrlCreate(
+        slug=slug,
+        description="A short url",
+        target_url="https://example.com",
+    )
+    return storage.create(short_url_in)
+
+
+@pytest.fixture(
+    params=[
+        "some-slug",
+        "slug",
+        "qwerty-abc",
+        pytest.param("abc", id="minimal-slug"),
+        pytest.param("qwerty-foo", id="max-slug"),
+    ],
+)
+def short_url(request: SubRequest) -> ShortUrl:
+    return create_short_url(request.param)
+
+
+def test_delete(
+    short_url: ShortUrl,
+    auth_client: TestClient,
+) -> None:
+    url = app.url_path_for(
+        "delete_short_url",
+        slug=short_url.slug,
+    )
+    response = auth_client.delete(url=url)
+    assert response.status_code == status.HTTP_204_NO_CONTENT, response.text
+    assert not storage.exists(short_url.slug), response.text
